65aec5ce81b6b3d2f077e5f20b6808568c85dec4
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-12-10 14:12:16 +0100

    added two new views, updated controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -7,21 +7,34 @@ namespace MvcMovie.Controllers
     {
         // GET: /HelloWorld/
 
-        public string Index()
+        // public string Index()
+        // {
+        //     return "This is my default action...";
+        // }
+
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
+
+        public IActionResult Welcome(string name, int numTimes){
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
 
         // GET: /HelloWorld/Welcome/
         // GET: /HelloWorld/Welcome?name=Kasiem&numtimes=4
         // Requires using System.Text.Encodings.Web;
 
-        public string Welcome(string name, int numTimes = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        // public string Welcome(string name, int numTimes = 1)
+        // {
+        //     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
 
-            // return "This is the Welcome action method...";
-        }
+        //     // return "This is the Welcome action method...";
+        // }
 
         // public string Welcome(string name, int ID = 1)
         // {
